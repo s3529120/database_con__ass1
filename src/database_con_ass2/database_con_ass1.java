e740e2fb473f706127c77a8f894080e41ba695d7
Thomas J. Higgins <s3529120@studennt.rmit.edu.au>
2018-03-20 21:08:02 +1100

    fixing git error

--- src/database_con_ass2/database_con_ass1.java
+++ src/database_con_ass2/database_con_ass1.java
@@ -5,31 +5,75 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class database_con_ass1 {
 
 	public static void main(String[] args) {
 		ArrayList<Business_Name> list;
-		if(args.length<1) {
-			list=readCSV(args[1]);
-		}
+		Long start,end;
+		start=System.currentTimeMillis();
+		list=readCSV(args[0]);
+		end=System.currentTimeMillis();
+		System.out.println(end-start);
 
 	}
 
 	
 	public static ArrayList<Business_Name> readCSV(String filename){
 		String line="";
-		String split=",";
+		String split="\\t";
 		ArrayList<Business_Name> list = new ArrayList<Business_Name>();
+		String bus_name,status;
+		Date reg_date;
+		Date cancel_date;
+		Date ren_date;
+		String form_state_num,pre_state_reg,abn;
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
 			while((line=br.readLine())!=null) {
 				String[] hold = line.split(split);
-				list.add(new Business_Name(hold[0],hold[1],new SimpleDateFormat("dd/MM/yyyy").parse(hold[2]),
-						new SimpleDateFormat("dd/MM/yyyy").parse(hold[3]),new SimpleDateFormat("dd/MM/yyyy").parse(hold[4]),hold[5],hold[6],hold[7]));
+				
+				pre_state_reg="";
+				abn="";
+				
+				bus_name=hold[1];
+				status=hold[2];
+				if(hold.length>6) {
+					form_state_num=hold[6];
+					if(hold.length>7) {
+						pre_state_reg=hold[7];
+						if(hold.length>8) {
+							abn=hold[8];
+						}else {
+							abn="";
+						}
+					}else {
+						pre_state_reg="";
+					}
+				}else {
+					form_state_num="";
+				}
+				if(hold[3].isEmpty()) {
+					reg_date=null;
+				}else {
+					reg_date=new SimpleDateFormat("dd/MM/yyyy").parse(hold[3]);
+				}
+				if(hold[4].isEmpty()) {
+					cancel_date=null;
+				}else {
+					cancel_date=new SimpleDateFormat("dd/MM/yyyy").parse(hold[4]);
+				}
+				if(hold[5].isEmpty()) {
+					ren_date=null;
+				}else {
+					ren_date=new SimpleDateFormat("dd/MM/yyyy").parse(hold[5]);
+				}
+				
+				list.add(new Business_Name(bus_name,status,reg_date,cancel_date,ren_date,form_state_num,pre_state_reg,abn));
 			}
 		}catch(Exception e) {
-			
+			e.printStackTrace();
 		}
 		return list;
 	}
