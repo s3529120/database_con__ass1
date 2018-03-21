70789b7c66010a316ce8e6e52ef792d712219aa3
rmit-s3529120-thomas-higgins <s3529120@student.rmit.edu.au>
2018-03-22 10:01:33 +1100

    Added schema and applied to Derby insertion

--- src/database_con_ass2/database_con_ass1.java
+++ src/database_con_ass2/database_con_ass1.java
@@ -97,7 +97,31 @@ public class database_con_ass1 {
 			Connection con= DriverManager.getConnection(derby);
 			Statement state=con.createStatement();
 			for(Business_Name name: list){
-				int res=state.executeUpdate("INSERT INTO names() VALUES("+name+")");
+				int res;
+				if(name.getAbn()!=null){
+					res=state.executeUpdate("INSERT INTO BusinessNumbers(name,abn) "
+							+ "VALUES("+name.getBus_name()+","+name.getAbn()+")");
+				}
+				if(name.getStatus()!=null){
+					res=state.executeUpdate("INSERT INTO RegistrationStatus(name,status) "
+							+ "VALUES("+name.getBus_name()+","+name.getStatus()+")");
+				}
+				if(name.getReg_date()!=null){
+					res=state.executeUpdate("INSERT INTO RegistrationDates(name,registrationDate) "
+							+ "VALUES("+name.getBus_name()+","+name.getReg_date()+")");
+				}
+				if(name.getRen_date()!=null){
+					res=state.executeUpdate("INSERT INTO RenewalDates(name,renewalDate) "
+							+ "VALUES("+name.getBus_name()+","+name.getRen_date()+")");
+				}
+				if(name.getCancel_date()!=null){
+					res=state.executeUpdate("INSERT INTO CancellationDates(name,cancelDate) "
+							+ "VALUES("+name.getBus_name()+","+name.getCancel_date()+")");
+				}
+				if(name.getPre_state_reg()!=null||name.getForm_state_num()!=null){
+					res=state.executeUpdate("INSERT INTO FormerStates(name,formerStateNum,PreviousRegState) "
+							+ "VALUES("+name.getBus_name()+","+name.getForm_state_num()+","+name.getPre_state_reg()+")");
+				}
 			}
 			state.close();
 		}catch(Exception e){
